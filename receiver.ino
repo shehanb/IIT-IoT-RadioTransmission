c19a0aa0c7f2ffb0017841c784b7733b5c9e5c04
shehanbhavan <moonwalkerdugs@gmail.com>
2016-06-14 13:40:10 +0530

    RadioTransmission

---
+++ receiver.ino
@@ -0,0 +1,42 @@
+#include <VirtualWire.h>
+
+const int led_pin = 13;
+const int receive_pin = 11;
+
+void setup()
+{
+    delay(1000);
+    Serial.begin(9600);	// Debugging only
+    Serial.println("Receiving Data from Radio Transmission");
+
+    // Initialise the IO and ISR
+    vw_set_rx_pin(receive_pin);
+    vw_set_ptt_inverted(true); // Required for DR3100
+    vw_setup(2000);	 // Bits per sec
+
+    vw_rx_start();       // Start the receiver PLL running
+
+    pinMode(led_pin, OUTPUT);
+}
+
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+	int i;
+
+        digitalWrite(led_pin, HIGH); // Flash a light to show received good message
+	// Message with a good checksum received, dump it.
+	
+	for (i = 0; i < buflen; i++)
+	{
+	    Serial.print(buf[i]-'0', HEX);
+	    Serial.print(' ');
+	}
+	Serial.println();
+        digitalWrite(led_pin, LOW);
+    }
+}
---
+++ transmitter.ino
@@ -0,0 +1,28 @@
+#include <VirtualWire.h>
+
+const int led_pin = 13;
+const int transmit_pin = 12;
+
+void setup()
+{
+    // Initialise the IO and ISR
+    vw_set_tx_pin(transmit_pin);
+    vw_set_ptt_inverted(true); // Required for DR3100
+    vw_setup(2000);       // Bits per sec
+    pinMode(led_pin, OUTPUT);
+}
+
+byte count = 1;
+
+void loop()
+{
+  char msg[7] = {'0','7','7','1','0','0','7'};
+
+  msg[6] = count;
+  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
+  vw_send((uint8_t *)msg, 7);
+  vw_wait_tx(); // Wait until the whole message is gone
+  digitalWrite(led_pin, LOW);
+  delay(1000);
+  count = count + 1;
+}
